d1bb84d33a85719385358e4df2888f0423709af8
Nikita Astrakhantsev <nikita.astronaut@gmail.com>
2016-12-25 21:16:49 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+token = '154355460:AAGfXyDClkBEfF9baw4eZw-rapYbEx-jCxA'
+
+response_text_on_start = 'Are you so lazy that you don\'t wanna open Google to search? I will help you! Just type your google search query here and the number of links you wish to see...\nSo, to begin with, what\'s your query? (only English, please!)'
+ask_for_number = 'Now please tell me how many first links you wanna see?'
+thank = 'Are you satisfied now? Maybe search for something else?'
+angry = 'Are you kidding me man? Gimme something that looks mole like an integer, do you copy?'
+not_english = 'Haven\'t I ask you to write in English? Do you speak English? Have you watched the Pulp Fiction?'
